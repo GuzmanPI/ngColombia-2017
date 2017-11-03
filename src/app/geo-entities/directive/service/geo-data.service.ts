ad44b732ef9fa7c0b08e9af0e29ca30988ea5292
Israel Guzman <isragp4@gmail.com>
2017-11-03 11:43:38 -0600

    Updates RxJS iports

--- src/app/geo-entities/directive/service/geo-data.service.ts
+++ src/app/geo-entities/directive/service/geo-data.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
 
-import 'rxjs/add/operator/map';
+import 'rxjs/operators';
 
 import {environment} from '../../../../environments/environment';
 import {Country, State, City} from '../../../data-models/geo-entities.model';
